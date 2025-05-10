df2848ec265d441bc4e983b8f24a3adff02341c1
Maura <meighanmaura@gmail.com>
2025-05-10 14:38:33 -0500

    Add files via upload

--- Medical Insurance Project Using Python.py
+++ Medical Insurance Project Using Python.py
@@ -20,3 +20,20 @@ print(omar_message)
 
 # Calculate the difference between Maria's and Omar's insurance costs
 maria_omar_difference = calculate_cost_difference(maria_insurance_cost, omar_insurance_cost)
+
+# Function to analyze smoking status
+def analyze_smoker(smoker_status):
+    if smoker_status == 1:
+        print("To lower your cost, you should consider quitting smoking.")
+    else:
+        print("Smoking is not an issue for you.")
+
+# Function to estimate insurance cost
+def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
+    estimated_cost = 400 * age - 128 * sex + 425 * num_of_children + 10000 * smoker - 2500
+    print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+    analyze_smoker(smoker)
+    return estimated_cost
+
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name='Keanu', age=29, sex=1, num_of_children=3, smoker=1)
